e567f1242ff19b154cba238f88eedf851f66fe6c
gdany91 <you@example.comdgarofalo52@gmail.com>
2023-02-15 14:34:28 +0100

    relazione many-to-many (non funzionante)

--- database/migrations/2023_02_15_102034_create_categories_table.php
+++ database/migrations/2023_02_15_102034_create_categories_table.php
@@ -8,6 +8,8 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     * 
+     * @return void
      */
     public function up()
     {
@@ -22,6 +24,8 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
+     * 
+     * @return void
      */
     public function down()
     {
--- database/migrations/2053_02_15_101901_add_foreign_keys.php
+++ database/migrations/2053_02_15_101901_add_foreign_keys.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table -> foreignId('typology_id') -> constrained();
                    
         });
-/* 
+
         Schema::table('category_product', function (Blueprint $table) {
             
             $table -> foreignId('category_id')
@@ -28,20 +28,27 @@ return new class extends Migration
                    -> constrained();
 
 
-        }); */
+        });
 
     }
 
     /**
      * Reverse the migrations.
+     * 
+     * @return void
      */
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            
-            $table -> foreignId('typology_id')
-                   -> constrained();
-    });
 
-  }
+            $table -> dropForeign('products_typology_id_foreign');
+        });
+        
+        Schema::table('category_product', function (Blueprint $table) {
+
+            $table -> dropForeign('category_product_category_id_foreign');
+            $table -> dropForeign('category_product_product_id_foreign');
+        });
+
+    }
 };
